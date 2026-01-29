be031b47a4e4121fea34560b7cd3ef82cb42baa0
ibruno <bruno@maciel.com>
2026-01-29 14:42:36 -0300

    chore: bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kuest-py-order-utils",
-    version="0.3.2",
+    version="0.3.3",
     author="Kuest Engineering",
     author_email="engineering@kuest.com",
     maintainer="Kuest Engineering",
